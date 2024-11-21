c9507bd8900dbf5608335e5275cfa5c6adf148d1
mzikri0604200 <muhzikri06@gmail.com>
2024-11-21 10:26:29 +0700

    perapihan Welcome Start

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,7 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import StartWelcome from './StartWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-
-    <h1 class="text-3xl font-bold underline text-green-500">Hello world!</h1>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <StartWelcome />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
