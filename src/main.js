7e873389cde06d6f0c4d6f54d4824602665d5a9f
mzikri0604200 <muhzikri06@gmail.com>
2024-11-26 11:22:43 +0700

    add install primevue with tailwind css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import './assets/style.css'
+import PrimeVue from 'primevue/config'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(PrimeVue, {
+    theme: 'none',
+  })
+  .mount('#app')
