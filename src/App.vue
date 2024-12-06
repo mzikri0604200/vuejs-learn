f985a9b3e4e10ecbb6664898f57beb75379d24ee
mzikri0604200 <muhzikri06@gmail.com>
2024-12-06 17:20:13 +0700

    Update Composition Api

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,6 @@ const greeting = 'Hello World'
   <!-- <HeroSection /> -->
   <!-- <DataTable1 /> -->
   <!-- <MainForm /> -->
-  <OptionsApi />
+  <!-- <OptionsApi /> -->
   <CompositionApi />
 </template>
--- src/components/learn/CompositionApi.vue
+++ src/components/learn/CompositionApi.vue
@@ -1,18 +1,27 @@
 <script setup>
 const name = 'Balitower Balifiber'
-const idH1 = 'satu'
+const styleH1 = 'text-4xl font-semibold'
+const btnDisabled = true
+const btnAttr = {
+  disabled: false,
+  type: 'button',
+  class: 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-30',
+}
 
 const showName = () => console.log(name)
 </script>
 <template>
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <h1 :id="idH1" class="text-4xl font-semibold">{{ name }}</h1>
-
-    <button
-      @click="showName"
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
-      Show Name Composition Api
-    </button>
+    <h1 :class="styleH1">{{ name }}</h1>
+    <div class="grid grid-cols-1 gap-3">
+      <button
+        :disabled="btnDisabled"
+        @click="showName"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-30"
+      >
+        Show Name Composition Api 1
+      </button>
+      <button @click="showName" v-bind="btnAttr">Show Name Composition Api 2</button>
+    </div>
   </div>
 </template>
