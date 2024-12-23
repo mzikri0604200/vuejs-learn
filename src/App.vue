97d1a042499cad7aef9c908587bae019ec386228
mzikri0604200 <muhzikri06@gmail.com>
2024-12-23 17:14:20 +0700

    ref and reactive

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,14 @@ import MainButton from './components/learn/MainButton.vue'
 // import ColumnGroup from 'primevue/columngroup' // optional
 // import Row from 'primevue/row' // optional
 const greeting = 'Hello World'
+import { ref, reactive } from 'vue'
+
+const user = reactive({ name: 'Balitower', age: 20 })
+
+const count = ref(1)
+function increment() {
+  count.value++
+}
 </script>
 
 <template>
@@ -20,4 +28,23 @@ const greeting = 'Hello World'
   <MainButton :title="greeting" />
   <MainButton :title="`Submit`" :disabled="true" />
   <MainButton />
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <button
+      @click="increment"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-30"
+    >
+      {{ count }}
+    </button>
+
+    <div>
+      {{ user.name }}
+
+      <button
+        @click="user.age++"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-30"
+      >
+        {{ user.age }}
+      </button>
+    </div>
+  </div>
 </template>
