6bf68650beb574d990625b227aa5e388af8ce16e
mzikri0604200 <muhzikri06@gmail.com>
2025-01-30 13:42:38 +0700

    Learn Slots

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@ import ListRendering from './components/learn/ListRendering.vue'
 import EventHandling from './components/learn/EventHandling.vue'
 import GetAPIpublic from './components/learn/GetAPIpublic.vue'
 import FormHandling from './components/learn/FormHandling.vue'
+import SlotButton from './components/learn/SlotsCard.vue'
+import SlotsCard from './components/learn/SlotsCard.vue'
 // import ColumnGroup from 'primevue/columngroup' // optional
 // import Row from 'primevue/row' // optional
 const greeting = 'Hello World'
@@ -34,5 +36,16 @@ const greeting = 'Hello World'
   <!-- <ListRendering /> -->
   <!-- <EventHandling /> -->
   <!-- <GetAPIpublic /> -->
-  <FormHandling />
+  <!-- <FormHandling /> -->
+  <SlotsCard>
+    <template #image>
+      <img class="rounded-t-lg" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="" />
+    </template>
+    <template #title> Noteworthy technology acquisitions 2021 </template>
+    <template #body>
+      Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse
+      chronological order.
+    </template>
+    <span>Read Me</span>
+  </SlotsCard>
 </template>
