d920e916a72f81f3ebb6bf31b496480fe5892995
mzikri0604200 <muhzikri06@gmail.com>
2025-02-13 14:15:27 +0700

    update main button

--- src/components/learn/MainButton.vue
+++ src/components/learn/MainButton.vue
@@ -12,13 +12,11 @@ const { title, disabled = false } = defineProps({
 })
 </script>
 <template>
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <button
-      :disabled
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-30"
-    >
-      <!-- {{ props.title }} -->
-      {{ title }}
-    </button>
-  </div>
+  <button
+    :disabled
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:opacity-30 text-xs"
+  >
+    <!-- {{ props.title }} -->
+    {{ title }}
+  </button>
 </template>
