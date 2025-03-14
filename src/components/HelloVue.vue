15c6ff146c2dbe83e50dce60310809878d6a2e70
mzikri0604200 <muhzikri06@gmail.com>
2025-03-14 14:05:51 +0700

    add hello component dan style component

---
+++ src/components/HelloVue.vue
@@ -0,0 +1,26 @@
+<script setup>
+console.info('Hello Vue');
+
+const data = {
+  id: 'hello',
+  class: 'hello',
+  message: '<h4 style="color:white;">Hello Message</h4>',
+};
+const btnDisabled = false;
+</script>
+
+<template>
+  <h1 v-bind="data">{{ data.message }}</h1>
+  <div :class="data.class" v-html="data.message.toUpperCase()"></div>
+  <button :disabled="btnDisabled">{{ data.class }}</button>
+</template>
+
+<style>
+.hello {
+  color: rgb(130, 255, 249);
+}
+
+body {
+  background-color: black;
+}
+</style>
---
+++ src/components/Style.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref } from 'vue';
+
+const red = ref(false);
+const bold = ref(false);
+
+const style1 = {
+  color: 'green',
+};
+
+const style2 = {
+  textTransform: 'uppercase',
+  fontWeight: 'bold',
+};
+
+function toggleRed() {
+  red.value = !red.value;
+}
+
+function toggleBold() {
+  bold.value = !bold.value;
+}
+</script>
+<template>
+  <h1 :class="{ red: red, bold: bold }">Style</h1>
+  <h1 :class="['red', 'bold']">Style</h1>
+
+  <h1 :style="[style1, style2]">Style</h1>
+  <button v-on:click="toggleRed">Red</button>
+  <button v-on:click="toggleBold">Bold</button>
+</template>
+<style scoped>
+.red {
+  color: red;
+}
+
+.bold {
+  text-transform: uppercase;
+  font-weight: bold;
+}
+</style>
