d411652a0220ef694ff4409283a3efc80e242c5e
mzikri0604200 <muhzikri06@gmail.com>
2025-07-09 14:44:37 +0700

    Component Instance in ButtonApp and Container

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,23 +1,26 @@
 <script setup>
 import { ref, useSlots } from 'vue';
 
-const slots = useSlots();
+// Diupdate 09/07/2025 - Component Instance
+// https://vuejs.org/api/component-instance.html#data
+
+// const slots = useSlots();
 
 const counter = ref(0);
 </script>
 
 <template>
-  <div v-if="slots.header">
+  <div v-if="$slots.header">
     <slot name="header" :counter="counter">
       <h1>Default Header</h1>
     </slot>
   </div>
-  <div v-if="slots.default">
+  <div v-if="$slots.default">
     <slot name="default" :counter="counter">
       <h5>Anda belum mengisi content</h5>
     </slot>
   </div>
-  <div v-if="slots.footer">
+  <div v-if="$slots.footer">
     <slot name="footer" :counter="counter">
       <p>Default Footer</p>
     </slot>
